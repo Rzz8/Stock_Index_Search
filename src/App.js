eb0c3bc041e47b7d4e6972f33170337c6e2efa25
Rzz8 <zhongruizhi@gmail.com>
2023-11-03 14:06:53 +1000

    update the api to fetch stocks in a specific industry

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { useFetchStock } from "./apis/api";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-balham.css";
+import SearchBar from "./components/SearchBar";
+import { useState } from "react";
+import "./App.css";
 
 const table = {
   columns: [
@@ -11,34 +14,50 @@ const table = {
   ],
 };
 
+const currentDate = new Date();
+
+const day = currentDate.getDate();
+const month = currentDate.getMonth() + 1; // Adding 1 to the month to display it as 1-12.
+const year = currentDate.getFullYear();
+
 function App() {
-  const { loading, stocks, error } = useFetchStock();
+  const [search, setSearch] = useState("");
+
+  const { loading, stocks, error } = useFetchStock(search);
+
+  const submitHandler = (data) => {
+    setSearch(data);
+  };
 
   if (loading) {
     return <p>loading...</p>;
   }
 
-  if (error) {
+  if (error.hasError === true) {
     return <p>Something went wrong: {error.message}</p>;
   }
 
   return (
-    <div
-      className="ag-theme-balham"
-      style={{
-        height: "300px",
-        width: "600px",
-      }}
-    >
-      {/* {stocks.map((stock) => {
-        return stock.industry;
-      })} */}
-
-      <AgGridReact
-        columnDefs={table.columns}
-        rowData={stocks}
-        pagination={true}
-      />
+    <div className="center">
+      Home | Stocks
+      <div>
+        <SearchBar onSubmit={submitHandler} />
+        <div>Industry: {search}</div>
+      </div>
+      <div>Showing stocks for {`${day}/${month}/${year}`}</div>
+      <div
+        className="ag-theme-balham"
+        style={{
+          height: "300px",
+          width: "630px",
+        }}
+      >
+        <AgGridReact
+          columnDefs={table.columns}
+          rowData={stocks}
+          pagination={true}
+        />
+      </div>
     </div>
   );
 }
--- src/apis/api.jsx
+++ src/apis/api.jsx
@@ -4,22 +4,39 @@ import React, { useEffect, useState } from "react";
 const URL = "https://aij1hx90oj.execute-api.ap-southeast-2.amazonaws.com/prod/";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-export const useFetchStock = () => {
+export const useFetchStock = (search) => {
   const [loading, setLoading] = useState(true);
   const [stocks, setStocks] = useState([]);
-  const [error, setError] = useState(false);
-
-  // const stocks = [
-  //   { symbol: "A", name: "A company", industry: "health" },
-  //   { symbol: "B", name: "B company", industry: "sport" },
-  //   { symbol: "C", name: "C company", industry: "tech" },
-  // ];
+  const [error, setError] = useState({
+    "hasError": false,
+    "status": "200",
+    "message": "Every thing is working"
+  });
 
   const getAllStocks = async () => {
     const url = URL + "all";
-    const response = await axios(url, { headers: { "x-api-key": API_KEY } });
 
+    const response = await axios(url, { headers: { "x-api-key": API_KEY } });
     return response.data;
+  };
+
+  const getSpecificStock = async (search) => {
+    const url = URL + `industry?industry=${search}`;
+
+    const response = await axios(url, { headers: { "x-api-key": API_KEY } });
+    const industryStocks = response.data;
+
+    const uniqueSymbols = new Set();
+
+    const uniqueStocks = industryStocks.filter((item) => {
+      if (!uniqueSymbols.has(item.symbol)) {
+        uniqueSymbols.add(item.symbol);
+        return true;
+      }
+      return false;
+    });
+
+    return uniqueStocks;
   };
 
   useEffect(() => {
@@ -29,11 +46,34 @@ export const useFetchStock = () => {
         setStocks(stocks);
         setLoading(false);
       } catch (error) {
-        setError(error);
+        setError({
+          "hasError": true,
+          "status": "400",
+          "message": "Something is not working"
+        });
         setLoading(false);
       }
     })();
   }, []);
+
+  useEffect(() => {
+    if (search) {
+      (async () => {
+        try {
+          const stocks = await getSpecificStock(search);
+          setStocks(stocks);
+          setLoading(false);
+        } catch (error) {
+          setError({
+            "hasError": true,
+            "status": "400",
+            "message": "Industry does not exist"
+          });
+          setLoading(false);
+        }
+      })();
+    }
+  }, [search]);
 
   return { loading, stocks, error };
 };
